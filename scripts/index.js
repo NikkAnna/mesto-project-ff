8d4236a8f80609a6142a116993128ebf0cdd579b
NikA <anna.nikulina1995@yandex.ru>
2024-03-11 18:41:17 +0300

    create, add and delete card function done, cards add on page

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function deleteCard(card) {
+  card.remove();
+};
+
+function addCard(card) {
+  placesList.append(card);
+}
+
+function createCard(link, name, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+  
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+  return cardElement;
+}
+
+initialCards.forEach(function(item) {
+  const card = createCard(item.link, item.name, deleteCard);
+  addCard(card);
+});
